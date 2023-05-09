46061e34a50694006b7322bcc851f1ee171a3b99
indra purnomo Aji <79568712+innoji26@users.noreply.github.com>
2023-05-09 23:46:01 +0700

    Update dbConfig.js

--- app/config/dbConfig.js
+++ app/config/dbConfig.js
@@ -4,6 +4,7 @@ const config = {
     user: process.env.MYSQLUSER,
     pass: process.env.MYSQLPASSWORD,
     db: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT,
     dialect: "mysql",
 };
 
