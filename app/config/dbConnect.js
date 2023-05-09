3c6402d34b538c27960431f3ae3a3fad27192df3
indra purnomo Aji <79568712+innoji26@users.noreply.github.com>
2023-05-09 23:46:40 +0700

    Update dbConnect.js

--- app/config/dbConnect.js
+++ app/config/dbConnect.js
@@ -4,6 +4,7 @@ const config = require("./dbConfig.js");
 const database = new Sequelize(config.db, config.user, config.pass, {
     host: config.host,
     dialect: config.dialect,
+    port: config.port,
 });
 
 const db = {};
